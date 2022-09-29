a0e5a49bd1f75612eb65132bb4b37afa2dd7efff
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-29 10:21:27 -0700

    add DOM el and window load listener and disp func

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
-import { createGroceryItem } from './fetch-utils.js';
+import { createGroceryItem, getGroceryItem } from './fetch-utils.js';
+import { renderGroceryItem } from './render-utils.js';
 
 /* Get DOM Elements */
 const createGroceryItemInputForm = document.getElementById('grocery-item-input-form');
 const errorDisplay = document.getElementById('error-display');
+const shoppingList = document.getElementById('shopping-list');
 
 /* State */
 let items = [];
 let error = null;
 
 /* Events */
+window.addEventListener('load', async () => {
+    const response = await getGroceryItem();
+
+    error = response.error;
+    items = response.data;
+
+    if (error) {
+        displayError();
+    } else {
+        displayGroceryItem();
+    }
+});
+
 createGroceryItemInputForm.addEventListener('submit', async (e) => {
     e.preventDefault();
 
@@ -22,8 +37,6 @@ createGroceryItemInputForm.addEventListener('submit', async (e) => {
         item: formData.get('item'),
     };
 
-    console.log(newItem);
-
     const response = await createGroceryItem(newItem);
     error = response.error;
     const item = response.data;
@@ -32,9 +45,9 @@ createGroceryItemInputForm.addEventListener('submit', async (e) => {
         displayError();
     } else {
         items.push(item);
+        displayGroceryItem();
+        createGroceryItemInputForm.reset();
     }
-
-    createGroceryItemInputForm.reset();
 });
 
 /* Display Functions */
@@ -42,4 +55,12 @@ function displayError() {
     // eslint-disable-next-line no-console
     console.error(error);
     errorDisplay.textContent = error.message;
+}
+
+function displayGroceryItem() {
+    shoppingList.innerHTML = '';
+    for (const item of items) {
+        const itemEl = renderGroceryItem(item);
+        shoppingList.append(itemEl);
+    }
 }
