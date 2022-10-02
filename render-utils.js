3c43aff2de17aca3fdd39ebde0a42f5f5a5cf715
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-10-02 10:58:25 -0700

    add button to render function

--- render-utils.js
+++ render-utils.js
@@ -1,10 +1,31 @@
+import { displayGroceryItem } from './app.js';
+import { deleteSingleItem } from './fetch-utils.js';
+
+let error = null;
+
 export function renderGroceryItem(item) {
     const li = document.createElement('li');
+
+    const button = document.createElement('button');
+    button.classList.add('delete-single-item');
+    button.textContent = 'Remove Item';
+
+    button.addEventListener('click', async () => {
+        const response = await deleteSingleItem(item.id);
+
+        error = response.error;
+
+        if (error) {
+            error = error.message;
+        } else {
+            displayGroceryItem();
+        }
+    });
 
     const p = document.createElement('p');
     p.textContent = `${item.quantity} ${item.item}`;
 
-    li.append(p);
+    li.append(p, button);
 
     return li;
 }
